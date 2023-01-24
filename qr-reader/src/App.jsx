13c4c76afbadf9468214ddff6a0339a79be71a51
bodimarci <94900714+bodimarci@users.noreply.github.com>
2023-01-24 13:36:48 +0100

    Phone ready

--- qr-reader/src/App.jsx
+++ qr-reader/src/App.jsx
@@ -3,10 +3,10 @@ import image from "./assets/image-qr-code.png";
 function App() {
   return (
     <section className="grid h-screen place-items-center ">
-      <div className="bg-white rounded-[25px] w-[400px] drop-shadow-xl">
-        <img src={image} alt="" className="w-[400px] p-5 rounded-[32px]" />
+      <div className="bg-white rounded-[25px] sm:w-[400px] w-[300px] drop-shadow-xl">
+        <img src={image} alt="" className="sm:w-[400px] w-[375px] p-5 rounded-[32px]" />
         <div className="text-center text-[#073A74] my-3">
-          <h1 className="text-3xl font-[700] font-outfit">
+          <h1 className="sm:text-3xl text-xl font-[700] font-outfit">
             Improve your front-end <br /> skills by building projects
           </h1>
           <p className="text-[#7B879D] font-[400] mt-4 mb-6 text-[15px] font-outfit">
